b34c25803a888b09644c33fc915842187ba44e1c
Anastasia <an.afalin@yandex.ru>
2022-06-29 23:16:15 +0300

    add CaesarCipher

---
+++ src/CaesarCipher.java
@@ -0,0 +1,31 @@
+public class CaesarCipher {
+    private static final String ALPHABET_PART_ONE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\":-!? +-*/\\@#$%^&(){}[];'|`~=_©«»—0123456789";
+    private static final String ALPHABET_PART_TWO = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\":-!? +-*/\\@#$%^&(){}[];'|`~=_©«»—0123456789";
+    private static final String ALPHABET = ALPHABET_PART_ONE + ALPHABET_PART_TWO;
+
+    public String encrypt(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char aChar : message.toCharArray()) {
+            int indexPosition = ALPHABET.indexOf(aChar);
+            int newIndexPosition;
+            char newCharAt = 0;
+            if (indexPosition > 0) {
+                if (key > 0) {
+                    newIndexPosition = (indexPosition + key) % ALPHABET.length() / 2;
+                } else {
+                    newIndexPosition = (indexPosition + ALPHABET.length() / 2 + (key % ALPHABET.length() / 2)) % ALPHABET.length();
+                }
+                newCharAt = ALPHABET.charAt(newIndexPosition);
+            }
+            result.append(newCharAt);
+        }
+        return result.toString();
+    }
+
+    public String deEncrypt(String message, int key) {
+        return encrypt(message, -1 * key);
+    }
+
+}
