e54622142cf06cdf810bc7972b66cb7735b615e7
Anastasia <an.afalin@yandex.ru>
2022-07-10 21:23:17 +0300

    add class "Menu" with method main

---
+++ src/Menu.java
@@ -0,0 +1,33 @@
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Здравствуйте! " +
+                    "Выберите:\n" +
+                    "1. Зашифровть файл\n" +
+                    "2. Расшифровать файл\n" +
+                    "3. Перебор\n" +
+                    "4. Синтаксический анализ\n" +
+                    "5. Выход");
+            String answer = scanner.nextLine();
+            switch (answer){
+                case ("1") -> new Coder().coder();
+                case ("2") -> new Decoder().decoder();
+                case ("3") -> new Bruteforce().bruteforce();
+                case ("4") -> System.out.println();
+            }
+
+            if(answer.equals("5")){
+                break;
+            }
+        }
+
+
+
+    }
+
+}
